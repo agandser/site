05a17e2420ab50b2b7f00fbf09eb9f4da0ca9cbe
Agandser <44395783+agandser@users.noreply.github.com>
2022-03-09 00:02:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,23 @@ include ('inc/functions.php');
 
 
 if($_GET['action'] == "out") {
-session start ();
-Include ('inc/connect. php'):
-include ('inc/functions.php*)
-ClEfS GET 'action J
-out () :
-Cat (Login ())
-QUID
-4 SESSION 1d ]
-clse
-if (isset (F POSTI ' 10g d
-FELEOE N enter ():
-10 (count (ferror)
-GUID
-SESSION[ 14 ]:
-4 admin • 1s admin(4UID);
-include (' form. php);
+    out();
+}
+
+if (login()) {
+  $UID = $_SESSION['id'];
+} else {
+    if(isset($_POST['log_in'])) {
+        $error = enter();
+      
+        if (count($error) == 0) {
+            $UID = $_SESSION['id'];
+            $admin =is_admin($UID);
+        }
+    }
+}
+if($UID) {
+    include ('form.php');
+}
+
 ?>

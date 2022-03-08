5320a233bda98a4e2977c708dcfd248ef679b189
Agandser <44395783+agandser@users.noreply.github.com>
2022-03-08 23:51:05 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?
+include ('inc/connect.php');
+include ('inc/functions.php');
+
+
+if($_GET['action'] == "out") {
+session start ();
+Include ('inc/connect. php'):
+include ('inc/functions.php*)
+ClEfS GET 'action J
+out () :
+Cat (Login ())
+QUID
+4 SESSION 1d ]
+clse
+if (isset (F POSTI ' 10g d
+FELEOE N enter ():
+10 (count (ferror)
+GUID
+SESSION[ 14 ]:
+4 admin • 1s admin(4UID);
+include (' form. php);
+?>
